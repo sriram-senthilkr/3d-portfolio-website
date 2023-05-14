8dd8ff5f144645f2aecf135145b288ee264044e1
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-05-14 21:52:17 +0800

    Add SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
+import Tilt from "react-parallax-tilt";
+
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ title, icon, index }) => {
+	return (
+		<Tilt className="xs:w-[250px] w-full">
+			<motion.div
+				variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+				className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+			>
+				<div
+					options={{
+						max: 45,
+						scale: 1,
+						speed: 450,
+					}}
+					className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+				>
+					<img src={icon} alt={title} className="w-16 h-15 objects-contain" />
+					<h3 className="text-white font-bold text-[20px] text-center">
+						{title}
+					</h3>
+				</div>
+			</motion.div>
+		</Tilt>
+	);
+};
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview.</h2>
+			</motion.div>
 
-export default About
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+			>
+				I am a full-stack web developer with a passion for creating beautiful,
+				responsive, and user-friendly websites and applications. I have a strong
+				foundation in HTML, CSS, JavaScript, and React. I am also familiar with
+				Node.js, Express, MongoDB, and Firebase. I am currently learning
+				TypeScript and Next.js.
+			</motion.p>
+			<div className="mt-20 flex flex-wrap gap-10">
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(About, "about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF("./desktop_pc/scene.gltf", true);
 
-export default Computers
+	return (
+		<mesh>
+			<hemisphereLight intensity={0.15} groundColor="black" />
+			<pointLight intensity={1} />
+			<primitive
+				object={computer.scene}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
+		</mesh>
+	);
+};
+
+const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+		setIsMobile(mediaQuery.matches);
+
+		const handleMedioQueryChange = (event) => {
+			setIsMobile(event.matches);
+		};
+
+		mediaQuery.addEventListener("change", handleMedioQueryChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", handleMedioQueryChange);
+		};
+	}, []);
+
+	return (
+		<Canvas
+			frameLoop="demand"
+			shadows
+			camera={{ position: [20, 3, 5], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}
+		>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Computers isMobile={isMobile} />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	);
+};
+
+export default ComputersCanvas;
