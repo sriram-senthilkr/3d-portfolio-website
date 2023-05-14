5dd38d719d001220cc77be0b402ae40461625947
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-05-14 23:48:24 +0800

    Added Earth model

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,6 @@ const Navbar = () => {
 					<img src={logo} alt="logo" className="w-9 h-9 object-contain" />
 
 					<p className="text-white text-[18px] font-bold cursor-pointer">
-						{" "}
 						sriram.
 					</p>
 				</Link>
